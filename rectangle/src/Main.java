ec241b49f8ab4f0169add592c77032c7e55032d4
Vadim Nastoiashchyi <58398016+VadimNastoyashchy@users.noreply.github.com>
2020-01-19 12:16:41 +0200

    Update Main.java
    Hello, I change your name class.

--- rectangle/src/Main.java
+++ rectangle/src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Rectangle kvadrat = new Rectangle(10);
-        Rectangle pryamokytnik = new Rectangle(20, 10);
+        Rectangle square = new Rectangle(10);
+        Rectangle rectangle = new Rectangle(20, 10);
 
-        System.out.println("Площа прямокутника = " + pryamokytnik.square());
-        System.out.println("Периметр прямокутника = " + pryamokytnik.perimeter());
+        System.out.println("Площа прямокутника = " + rectangle.square());
+        System.out.println("Периметр прямокутника = " + rectangle.perimeter());
     }
 }
